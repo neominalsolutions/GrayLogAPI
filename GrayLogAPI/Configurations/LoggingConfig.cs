6c73b670296e64f532afaad55abe36c6f9cdc148
mert.alptekin <mert.alptekin@neominal.com>
2022-03-21 23:40:55 +0300

    Add project files.

---
+++ GrayLogAPI/Configurations/LoggingConfig.cs
@@ -0,0 +1,47 @@
+﻿using Serilog;
+
+namespace GrayLogAPI.Configurations
+{
+    public class LoggingConfig
+    {
+
+        private static string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+        public static IConfiguration configuration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/appsettings.json", optional: false)
+                    .AddJsonFile($"Configurations/appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        public static IConfiguration serilogConfiguration
+        {
+            get
+            {
+                return new ConfigurationBuilder()
+                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .AddJsonFile($"Configurations/serilog.json", optional: false)
+                    .AddJsonFile($"Configurations/serilog.{env}.json", optional: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+            }
+        }
+
+        public static void ConfigureLogging()
+        {
+
+            Log.Logger = new LoggerConfiguration()
+               .Enrich.FromLogContext()
+               .ReadFrom.Configuration(serilogConfiguration)
+               .CreateLogger();
+
+        }
+
+    }
+}
---
+++ GrayLogAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrayLogAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var correlationID = Guid.NewGuid();
+
+            _logger.LogInformation("WeatherForecast GET {CorrelationID}", correlationID);
+
+
+            try
+            {
+                int number = 5;
+                int result = number / 0;
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError("{CorrelationID} {MessageTemplate}", correlationID, ex.Message);
+
+                throw;
+            }
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
